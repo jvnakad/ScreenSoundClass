35b6794a9b13794e6a5b7ba5db07c0ff67b6a416
jvnakad <joaonakad@gmail.com>
2024-09-08 21:33:17 -0300

    Adição da Classe Musica e Album

--- ScreenSoundClass/Program.cs
+++ ScreenSoundClass/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Album albumDoColdplay = new Album();
+albumDoColdplay.Nome = "A Rush of Blood to the Head";
+
+Musica musica1 = new Musica();
+musica1.Nome = "The Scientist";
+musica1.Duracao = 213;
+
+Musica musica2 = new Musica();
+musica2.Nome = "Clocks";
+musica2.Duracao = 350;
+
+albumDoColdplay.AdicionarMusica(musica1);
+albumDoColdplay.AdicionarMusica(musica2);
+
+albumDoColdplay.ExibirMusicasDoAlbum();
